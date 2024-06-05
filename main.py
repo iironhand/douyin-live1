3ee40a38d6f3aa0ccd20be1d903ecb44fff67021
iironhand <slow66@qq.com>
2024-06-05 10:33:20 +0800

    1

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ from src.douyin_live_info import start_get_dy_user
 from src.utils.common import init_global
 
 
-def barrage(args, mloop):
-    if not (len(args) >= 2 and args[1].startswith("https://live.douyin.com/")):
-        raise Exception("参数错误")
-    room_url = args[1]
-
+def barrage(room_url, mloop):
     asyncio.set_event_loop(mloop)
     logging.basicConfig(level=logging.ERROR)
     init_global()
@@ -23,11 +19,25 @@ def barrage(args, mloop):
 
 
 if __name__ == "__main__":
-    # program_args = sys.argv
-    program_args = (0, "https://live.douyin.com/250844044426")
+    args = sys.argv
+    for arg in args:
+        print(arg)
+    print("------------------")
+
+    print(len(args))
+    room_url = args[1]
+    open_avatar_thread = args[2]
+
+    if not args[1].startswith(r"https://live.douyin.com/"):
+        print("请输入正确的房间地址")
+        exit(0)
+
+    # program_args = (0, "https://live.douyin.com/250844044426")
 
     loop = asyncio.new_event_loop()
     threading.Thread(target=local_server.start_server, args=(loop,)).start()
-    threading.Thread(target=start_get_dy_user, args=(program_args[1],loop)).start()
 
-    barrage(program_args, loop)
+    if open_avatar_thread == "true":
+        threading.Thread(target=start_get_dy_user, args=(room_url, loop)).start()
+
+    barrage(room_url, loop)
